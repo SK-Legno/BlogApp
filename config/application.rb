d1b8c895413054a0c1244e6554d4874a1c27016e
legno <legno.leon@gmail.com>
2025-04-12 22:40:14 +0900

    gemfileの編集とデータベースのユーザ・パスワード環境変数設定変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    # Load environment variables from .env files
+    if defined?(Dotenv)
+      Dotenv.load(".env", ".env.#{Rails.env}")
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
